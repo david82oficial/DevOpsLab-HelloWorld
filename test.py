243c1aaf59119349f1f8deeb38c6c4d793f2e6f2
david.82 <david.82@hotmail.com>
2018-10-08 22:39:17 -0400

    Update README 2

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Teste do DevOps.")
+        self.assertRegex(result.data.decode(), "Teste do DevOps1.")
 
 
 if __name__ == "__main__":
